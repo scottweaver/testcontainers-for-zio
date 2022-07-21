9e64eb386ebbbb28b6bfc787c59ff81e5ed45f8d
Scott T Weaver <scott.t.weaver@gmail.com>
2022-07-21 15:47:57 -0500

    New spec of Container

--- modules/core/src/test/scala/io/github/scottweaver/zillen/ContainerSpec.scala
+++ modules/core/src/test/scala/io/github/scottweaver/zillen/ContainerSpec.scala
@@ -1,5 +1,12 @@
 package io.github.scottweaver.zillen
 
-object ContainerSpec {
-  
+import zio.test._
+
+object ContainerSpec extends ZIOSpecDefault {
+  val spec = suite("ContainerSpec")(
+    test("#scopedContainer should properly run the entire lifecycle of a container.") {
+      
+      assertTrue(true)
+   }
+  )
 }
