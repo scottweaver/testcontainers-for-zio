b79f54b871d00414a4cf76287d5b6430838602cc
Scott T Weaver <scott.t.weaver@gmail.com>
2022-08-05 08:38:13 -0500

    Added setting to turn off the welcome banner.

--- project/ZioEcosystemProjectPlugin.scala
+++ project/ZioEcosystemProjectPlugin.scala
@@ -30,6 +30,8 @@ object ZioEcosystemProjectPlugin extends AutoPlugin {
 
     val needsZio = settingKey[Boolean]("Indicates whether or not the project needs ZIO libraries.")
 
+    val welcomeBannerEnabled = settingKey[Boolean]("Indicates whether or not to enable the welcome banner.")
+
   }
 
   private def buildInfoSettings(packageName: String) =
@@ -80,11 +82,12 @@ object ZioEcosystemProjectPlugin extends AutoPlugin {
   import autoImport._
 
   def stdSettings: Seq[Setting[_]] = Seq(
-    crossScalaVersions := Seq(V.Scala212, V.Scala213, V.Scala3),
-    scalaVersion       := V.Scala213,
-    zioSeries          := ZIOSeries.Series2X,
-    needsZio           := true,
-    scalacOptions      := ScalaCompilerSettings.stdScalacOptions(scalaVersion.value),
+    crossScalaVersions   := Seq(V.Scala212, V.Scala213, V.Scala3),
+    scalaVersion         := V.Scala213,
+    zioSeries            := ZIOSeries.Series2X,
+    needsZio             := true,
+    welcomeBannerEnabled := true,
+    scalacOptions        := ScalaCompilerSettings.stdScalacOptions(scalaVersion.value),
     libraryDependencies ++= {
       if (needsZio.value)
         Seq(
@@ -109,47 +112,30 @@ object ZioEcosystemProjectPlugin extends AutoPlugin {
   )
 
   def welcomeMessage = onLoadMessage := {
-    import scala.Console
+    if (welcomeBannerEnabled.value) {
+      import scala.Console
 
-    def header(text: String): String = s"${Console.RED}$text${Console.RESET}"
+      def header(text: String): String = s"${Console.RED}$text${Console.RESET}"
 
-    def item(text: String): String    = s"${Console.GREEN}> ${Console.CYAN}$text${Console.RESET}"
-    def subItem(text: String): String = s"  ${Console.YELLOW}> ${Console.CYAN}$text${Console.RESET}"
+      def item(text: String): String    = s"${Console.GREEN}> ${Console.CYAN}$text${Console.RESET}"
+      def subItem(text: String): String = s"  ${Console.YELLOW}> ${Console.CYAN}$text${Console.RESET}"
 
-    // s"""|
-    //     |${header(" ________ ___")}
-    //     |${header("|__  /_ _/ _ \\")}
-    //     |${header("  / / | | | | |")}
-    //     |${header(" / /_ | | |_| |")}
-    //     |${header(s"/____|___\\___/   🐳 ${name.value} v.${version.value}")}
-    //     |Useful sbt tasks:
-    //     |${item("build")} - Checks source files for proper formatting, strictly compiles and runs tests.
-    //     |${item("check")} - Verifies that all source files are properly formatted.
-    //     |${item("prepare")} - Prepares sources by applying scalafmt.
-    //     |${item("fmt")} - Formats source files using scalafmt.
-    //     |${item("enableStrictCompile")} - Enables strict compilation e.g. warnings become errors.
-    //     |${item("disableStrictCompile")} - Disables strict compilation e.g. warnings are no longer treated as errors.
-    //     |${item("~compile")} - Compiles all modules (file-watch enabled)
-    //     |${item("test")} - Runs all tests
-    //     |${item("testOnly *.YourSpec -- -t \"YourLabel\"")} - Only runs tests with matching term e.g.
-    //     |${item("docs/docusaurusCreateSite")} - Generates the ZIO microsite
-    //   """.stripMargin
-
-    s"""|${Banner(s"🐳 ${name.value} v.${version.value}")}
-        |Useful sbt tasks:
-        |${item("build")} - Checks source files for proper formatting, strictly compiles and runs tests.
-        |${item("lint")} - Verifies that all source files are properly formatted, have the correct license headers and have had all scalafix rules applied.
-        |${item("prepare")} - Prepares sources by applying scalafmt, adding missing license headers snd running scalafix.
-        |${item("fmt")} - Formats source files using scalafmt.
-        |${item("fix")} - Fixes source files using using scalafix.
-        |${item("enableStrictCompile")} - Enables strict compilation e.g. warnings become errors.
-        |${item("disableStrictCompile")} - Disables strict compilation e.g. warnings are no longer treated as errors.
-        |${item("~compile")} - Compiles all modules (file-watch enabled)
-        |${item("test")} - Runs all tests
-        |${item("testOnly *.YourSpec -- -t \"YourLabel\"")} - Only runs tests with matching term e.g.
-        |${item("docs/docusaurusCreateSite")} - Generates the ZIO microsite
+      s"""|${Banner.trueColor(s"🐳 ${name.value} v.${version.value}")}
+          |Useful sbt tasks:
+          |${item("build")} - Lints source files then strictly compiles and runs tests.
+          |${item("lint")} - Verifies that all source files are properly formatted, have the correct license headers and have had all scalafix rules applied.
+          |${item("prepare")} - Prepares sources by applying scalafmt, adding missing license headers and running scalafix.
+          |${item("fmt")} - Formats source files using scalafmt.
+          |${item("fix")} - Fixes source files using using scalafix.
+          |${item("enableStrictCompile")} - Enables strict compilation e.g. warnings become errors.
+          |${item("disableStrictCompile")} - Disables strict compilation e.g. warnings are no longer treated as errors.
+          |${item("~compile")} - Compiles all modules (file-watch enabled)
+          |${item("test")} - Runs all tests
+          |${item("testOnly *.YourSpec -- -t \"YourLabel\"")} - Only runs tests with matching term e.g.
+          |${item("site")} - Generates the ZIO microsite
       """.stripMargin
 
+    } else ""
   }
 
   override def projectSettings: Seq[Setting[_]] = stdSettings ++ Tasks.settings ++ Commands.settings ++ welcomeMessage
